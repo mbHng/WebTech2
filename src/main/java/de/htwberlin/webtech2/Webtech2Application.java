ce99bfed0114fa71140602553e32af01a4f0b7eb
mbHng <buiminh2000@gmx.de>
2021-12-15 17:25:09 +0100

    first commit

---
+++ src/main/java/de/htwberlin/webtech2/Webtech2Application.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtech2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtech2Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech2/Webtech2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webtech2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
